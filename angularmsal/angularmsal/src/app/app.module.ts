8b2e8616c7e1787b732c4a264b6b2469de2de80f
fplaras <1815587@fplaras@users.noreply.github.com>
2020-09-16 13:50:46 -0400

    implemented angular msal

---
+++ angularmsal/angularmsal/src/app/app.module.ts
@@ -0,0 +1,61 @@
+  
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { Configuration } from 'msal';
+import {
+  MsalModule,
+  MsalInterceptor,
+  MSAL_CONFIG,
+  MSAL_CONFIG_ANGULAR,
+  MsalService,
+  MsalAngularConfiguration
+} from '@azure/msal-angular';
+
+import { msalConfig, msalAngularConfig } from './app-config';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+function MSALConfigFactory(): Configuration {
+  return msalConfig;
+}
+
+function MSALAngularConfigFactory(): MsalAngularConfiguration {
+  return msalAngularConfig;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MsalModule
+
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_CONFIG,
+      useFactory: MSALConfigFactory
+    },
+    {
+      provide: MSAL_CONFIG_ANGULAR,
+      useFactory: MSALAngularConfigFactory
+    },
+    MsalService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
