ac488ba510e84991f9dbf23d97e06edaf3af4c99
fplaras <1815587@fplaras@users.noreply.github.com>
2021-03-26 16:25:59 -0400

    updated paths

--- vue-firebase/vue-firebase/src/main.js
+++ vue-firebase/vue-firebase/src/main.js
@@ -4,10 +4,19 @@ import VueRouter from 'vue-router'
 import RouterPrefetch from 'vue-router-prefetch'
 // router setup
 import router from './routes/router';
+
+//Bootstrap
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/style.css'
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(RouterPrefetch)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 
 new Vue({
